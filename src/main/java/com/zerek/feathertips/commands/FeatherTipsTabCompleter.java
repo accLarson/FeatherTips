40fef87854f1401cc300bb7dba02133cb99d5d95
Derek <acc.larson@gmail.com>
2022-07-17 11:37:08 -0400

    Removed tab completing for /feathertips reload for non-op players.

--- src/main/java/com/zerek/feathertips/commands/FeatherTipsTabCompleter.java
+++ src/main/java/com/zerek/feathertips/commands/FeatherTipsTabCompleter.java
@@ -13,7 +13,7 @@ public class FeatherTipsTabCompleter implements TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         List<String> options = new ArrayList<>();
-        options.add(0,"reload");
+        if (sender.isOp()) options.add(0,"reload");
         return options;
     }
 }
